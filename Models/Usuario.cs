4316e2541b1124a6319cb111702ec9cd1df0d280
marceloS2 <82478461+marceloS2@users.noreply.github.com>
2022-09-02 14:02:34 -0300

    Add files via upload

---
+++ Models/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace WebPostgreSQL.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        [Column("Id")]
+        [Display(Name = "Codigo")]
+        public int Id { get; set; }
+
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("Senha")]
+        [Display(Name = "Senha")]
+        public int Senha { get; set; }
+
+
+
+    }
+}
